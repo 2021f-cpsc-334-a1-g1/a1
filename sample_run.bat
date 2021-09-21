532029efde0adb29b2c6e890b70e8dae9c792f05
Cody Neiman <neiman1010222@gmail.com>
2021-09-21 18:58:54 -0400

    Add mpv view to sample run

--- sample_run.bat
+++ sample_run.bat
@@ -1,6 +1,11 @@
 @echo off
 
-echo Running!
+echo Running process-video.py...
 .\bin\python\WPy64-3950\python-3.9.5.amd64\python.exe .\process_video.py .\reports\leeds\display-config.json .\demo_content\johnny-bubble.mp4 .\out.mp4 -v -r 270
+echo Running process-video.py...Complete
+pause
 
+echo Viewing output...
+.\bin\mpv\mpv.exe .\out.mp4 --no-border
+echo Viewing output...Complete
 pause
